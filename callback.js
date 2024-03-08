d3a34d65f50368979e1f6c3ac11540830238dabc
cerendn <cerendncer@outlook.com>
2024-03-08 21:05:17 +0300

    update

---
+++ callback.js
@@ -0,0 +1,21 @@
+const candles = [
+    { name : "Magic" , esans : "Paculi"},
+    { name : "Flower" , esans : "Çiçeksi"},
+    { name : "Coffee" , esans : "Kahve"},
+];
+
+const listCandles = () => {
+    candles.map((candle) => {
+        console.log(candle.name);
+    })
+};
+listCandles();
+
+const addCandle = (newCandle,callback) => {
+    candles.push(newCandle);
+    callback();
+};
+addCandle({name : "Rose", esans :"Gül"});
+
+
+
--- promise.js
+++ promise.js
@@ -4,10 +4,40 @@
 //});
 //console.log(promise1);
 
-// promise1.then(value => {
-//     console.log(value);
-// }) then ile resolve veri yakalandı
+// promise1
+//    .then(value => {
+//       console.log(value);
+//    }) //then ile resolve veri yakalandı
+//    .catch(error => {
+//       console.log(error);
+//    }); //hata catch metodu ile yakalandı
 
-// promise1.catch(error => {
-//     console.log(error);
-// }); hata catch metodu ile yakalandı
+const candles = [
+   { name : "Magic" , esans : "Paculi"},
+   { name : "Flower" , esans : "Çiçeksi"},
+   { name : "Coffee" , esans : "Kahve"},
+];
+
+const listCandles = () => {
+   candles.map((candle) => {
+       console.log(candle.name);
+   })
+};
+
+const addCandle = (newCandle) => {
+   const promise1 = new Promise((resolve, reject) => {
+      candles.push(newCandle);
+   });
+   return promise1;
+};
+
+addCandle({name : "Rose", esans :"Gül"})
+   .then(() => {
+      console.log("new candle");
+      listCandles();
+   })
+   .catch((error) => {
+      console.error(error);
+   });
+
+listCandles();
