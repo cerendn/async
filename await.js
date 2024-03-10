628103b401c820f45faaed98d2ab8304a3778b24
cerendn <cerendncer@outlook.com>
2024-03-10 23:21:08 +0300

    await2

--- await.js
+++ await.js
@@ -31,5 +31,33 @@
 //async-await
 const jspURI = "https://jsonplaceholder.typicode.com";
 
- const response = fetch(`${jspURI}/comments/1`);
+ //const response = fetch(`${jspURI}/comments/1`);
  console.log(response); //Promise pending
+ //const response = await fetch(`${jspURI}/comments/1`); //error
+ 
+ const getCommentsWithAwait = async () => {
+    console.log("inner await 1");
+    console.log("inner await 2");
+    const response = await fetch(`${jspURI}/comments/1`);
+    const data = await response.json();
+    console.log("await answer: ", data);
+    console.log("inner await 3");
+    console.log("inner await 4");
+}
+const getCommentsWithThen = async () => {
+    console.log("inner then 1");
+    console.log("inner then 2");
+    fetch(`${jspURI}/comments/1`)
+        .then(response => response.json())
+        .then(data => console.log("then answer: ", data));
+    console.log("inner then 3");
+    console.log("inner then 4");
+
+}
+
+console.log("outer 1");
+console.log("outer 2");
+getCommentsWithAwait();
+getCommentsWithThen();
+console.log("outer 3");
+console.log("outer 4");
