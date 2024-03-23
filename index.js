35a0cbc366129b6a373fed13c34269499a3fa0da
cerendn <cerendncer@outlook.com>
2024-03-23 18:34:39 +0300

    upd2

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-//Await
-// function doOperation() {
-//     return new Promise ((resolve,reject) => {
-//         setTimeout(() => {
-//             const random = Math.random();
-//             if(random < 0.7) {
-//                 resolve ("İşlem başarılı!");
-//             } else {
-//                 reject("İşlem başarısız!");
-//             }
-//         },1000);
-//     });
-// }
+Await
+function doOperation() {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            const random = Math.random();
+            if(random < 0.7) {
+                resolve ("İşlem başarılı!");
+            } else {
+                reject("İşlem başarısız!");
+            }
+        },1000);
+    });
+}
 
-// async function main() {
-//     try {
-//         console.log("İşlem başlatılıyor...");
-//         const result = await doOperation() ;
-//         console.log(result);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-// main();
 
  
